2c05ee9afe13f1b766e5f7f9ec1a90cd0e771049
Akaran19 <202208344@post.au.dk>
2025-09-03 00:49:21 +0200

    added UI change

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -255,7 +255,7 @@ export default function HomePage() {
 
           {/* Papers Grid */}
           {isLoading ? (
-            <div className="grid grid-cols-1 gap-4 md:gap-6">
+            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 md:gap-6">
               {[...Array(6)].map((_, i) => (
                 <PaperCardSkeleton key={i} showAbstract />
               ))}
@@ -268,7 +268,7 @@ export default function HomePage() {
               </div>
             </div>
           ) : (
-            <div className="grid grid-cols-1 gap-4 md:gap-6">
+            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 md:gap-6">
               {displayedPapers.map((paper) => (
                 <PaperCard
                   key={paper.id}
